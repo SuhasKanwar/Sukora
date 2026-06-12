44ca52a2399ebf1bd3518e9342f68a46f3ccc0ef
SuhasKanwar <suhas.kanwar@gmail.com>
2026-06-13 00:26:34 +0530

    added actions and message text listeners

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import { Markup, Telegraf } from 'telegraf';
 import dotenv from 'dotenv';
 import { TELEGRAM_BOT_TOKEN } from './lib/conifg';
+import { Keypair } from '@solana/web3.js';
+import { message } from 'telegraf/filters';
 
 dotenv.config();
 
 const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
+
+const USERS: Record<string, Keypair> = {};
+interface PendingRequestType {
+    type: "SEND_SOL" | "SEND_TOKEN",
+    amount?: number,
+    to?: string
+}
+const PENDING_REQUESTS: Record<string, PendingRequestType> = {}; 
 
 const keyboard = Markup.inlineKeyboard([
     [
         Markup.button.callback('Generate Wallet', 'generate_wallet'),
     ],
     [
-        Markup.button.callback('View Address', 'view_address'),
+        Markup.button.callback('Show Public Key', 'show_pub_key'),
         Markup.button.callback('Export Private Key', 'export_private_key')
     ],
     [
@@ -19,7 +29,7 @@ const keyboard = Markup.inlineKeyboard([
         Markup.button.callback('Transaction History', 'tx_history')
     ],
     [
-        Markup.button.callback('Send SOL', 'send_sol_menu'),
+        Markup.button.callback('Send SOL', 'send_sol'),
         Markup.button.callback('Send Token', 'send_token_menu')
     ]
 ])
@@ -34,6 +44,56 @@ bot.start(async (ctx) => {
         parse_mode: 'Markdown',
         ...keyboard
     });
+});
+
+bot.action('generate_wallet', async (ctx) => {
+    ctx.answerCbQuery("Generating new Wallet...");
+    const userId = ctx.from?.id;
+    const keypair = Keypair.generate();
+    USERS[userId] = keypair;
+
+    ctx.sendMessage(`New wallet created for you with the public key: ${keypair.publicKey.toBase58()}`)
+});
+
+bot.action('show_pub_key', async (ctx) => {
+    ctx.answerCbQuery("Getting your public key...");
+    const userId = ctx.from?.id;
+    const keypair = USERS[userId];
+    if(!keypair) {
+        ctx.sendMessage("You do not have a wallet with us yet, please click on the 'Generate Wallet' button first to create one", {
+            parse_mode: 'Markdown',
+            ...keyboard
+        });
+        return;
+    }
+
+    ctx.sendMessage(`Public Key: ${keypair.publicKey.toBase58()}`);
+});
+
+bot.action("send_sol", (ctx) => {
+    ctx.sendMessage("Can you share the address to send to...");
+    const userId = ctx.from?.id;
+    PENDING_REQUESTS[userId] = {
+        type: "SEND_SOL"
+    };
+});
+
+// TODO: Validation needs to be handeled
+bot.on(message("text"), async (ctx) => {
+    const userId = ctx.from?.id;
+    if(PENDING_REQUESTS[userId]?.type == "SEND_SOL") {
+        if(PENDING_REQUESTS[userId] && !PENDING_REQUESTS[userId].to) {
+            // TODO: Check whether the public key is valid or not
+            const toPubKey = ctx.message.text.toString().trim();
+            PENDING_REQUESTS[userId].to = toPubKey;
+            ctx.sendMessage("How much SOL do you want to send?");
+        } else {
+            const amount = ctx.message.text;
+            // TODO: Create a txn and forward it to the blockchain
+            ctx.sendMessage(`Initiated a txn for ${amount} SOL to ${PENDING_REQUESTS[userId].to}`);
+            delete PENDING_REQUESTS[userId];
+        }
+    }
 });
 
 async function startBot(): Promise<void> {
